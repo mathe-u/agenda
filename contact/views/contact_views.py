d6d89ca0f568e36dcef8afece21f3bd0d7c1d435
mathe-u <matheussilvades@gmail.com>
2025-02-16 18:21:43 -0300

    add view search to contact_views

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,9 +1,9 @@
 """Contact View"""
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
+from django.db.models import Q
 from contact.models import Contact
 
-# Create your views here.
 
 def index(request):
     """Main page"""
@@ -13,6 +13,34 @@ def index(request):
     context = {
         'contacts': contacts,
         'title': 'Contatos',
+    }
+
+    return render(
+        request=request,
+        template_name='contact/index.html',
+        context=context,
+    )
+
+def search(request):
+    """Main page"""
+
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    print(search_value)
+
+    contacts = Contact.objects.filter(show=True).filter( # pylint: disable=no-member
+        Q(first_name__icontains=search_value) |
+        Q(last_name__icontains=search_value) |
+        Q(phone__icontains=search_value) |
+        Q(email__icontains=search_value)
+        ).order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'title': 'Search',
     }
 
     return render(
