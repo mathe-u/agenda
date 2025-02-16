7a013e13e729f420ff5656e93a36da77e4dac4b0
mathe-u <matheussilvades@gmail.com>
2025-02-15 22:53:46 -0300

    create admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,24 @@
+"""Admin"""
+
 from django.contrib import admin
+from contact import models
 
 # Register your models here.
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    """Contact Admin"""
+    list_display = ('id', 'first_name', 'last_name', 'phone')
+    ordering = ('-id', )
+    #list_filter = ('created_date', )
+    search_fields = ('id', 'first_name', 'last_name')
+    list_per_page = 10
+    list_max_show_all = 100
+    list_editable = ('first_name', 'last_name')
+    list_display_links = ('id', 'phone')
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Contact Category"""
+    list_display = ('name',)
+    ordering = ('-id',)
