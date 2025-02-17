9c0f3bb7c0b9c2fea0318117963dc4080b3fcf96
mathe-u <matheussilvades@gmail.com>
2025-02-17 15:47:18 -0300

    add validation form

--- contact/forms.py
+++ contact/forms.py
@@ -7,21 +7,49 @@ from contact.models import Contact
 
 class ContactForm(forms.ModelForm):
     class Meta:
+        """Meta Class ContactForm"""
         model = Contact
         fields = (
             'first_name',
             'last_name',
             'phone',
+            'email',
+            'description',
+            'category',
         )
+
+        # widgets = {
+        #     'first_name': forms.TextInput(
+        #         attrs={
+        #             'class': 'classe',
+        #             'placeholder': 'Escreva aqui',
+        #         }
+        #     )
+        # }
 
     def clean(self):
-        cleaned_data = self.cleaned_data
+        # cleaned_data = self.cleaned_data
 
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Messagem de erro',
-                code='invalid'
-            )
-        )
+        # self.add_error(
+        #     'first_name',
+        #     ValidationError(
+        #         'Messagem de erro',
+        #         code='invalid'
+        #     )
+        # )
+
         return super().clean()
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Nao digite abc',
+                    code='invalid',
+                )
+            )
+
+        return first_name
